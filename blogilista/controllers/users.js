b58d9e3aa6bd739da82c00bbbd4bf2deba498d18
Oskari Juvakka <oskari.juvakka@gmail.com>
2023-03-30 20:19:16 +0300

    13.8: Add PUT request to update users name

--- blogilista/controllers/users.js
+++ blogilista/controllers/users.js
@@ -1,7 +1,11 @@
 require("express-async-errors");
 const router = require("express").Router();
 const { User } = require("../models");
-const { NotFoundError, NotUniqueError } = require("../utils/errors");
+const {
+  ValueError,
+  NotFoundError,
+  NotUniqueError,
+} = require("../utils/errors");
 
 router.get("/", async (req, res) => {
   const users = await User.findAll();
@@ -16,10 +20,15 @@ router.post("/", async (req, res) => {
   res.json(created);
 });
 
-router.get("/:id", async (req, res) => {
-  const id = req.params.id;
-  const user = await User.findByPk(id);
-  if (!user) throw new NotFoundError(`User: ${id}`);
+router.put("/:username", async (req, res) => {
+  const name = req.body.name;
+  const username = req.params.username;
+  if (!name) throw new ValueError("name");
+  const user = await User.findOne({ where: { username: username } });
+  if (!user) throw new NotFoundError(`User: ${username}`);
+  user.name = name;
+  const updated = await user.save();
+  res.json(updated);
 });
 
 module.exports = router;
