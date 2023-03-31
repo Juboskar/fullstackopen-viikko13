b4d9ec722ce60b5789913407286307ac710bd1f9
Oskari Juvakka <oskari.juvakka@gmail.com>
2023-03-31 13:42:52 +0300

    13.10: Add login routes, jsonwebtoken

--- blogilista/app.js
+++ blogilista/app.js
@@ -2,11 +2,13 @@ const express = require("express");
 const app = express();
 const blogsRouter = require("./controllers/blogs");
 const usersRouter = require("./controllers/users");
+const loginRouter = require("./controllers/login");
 const errorHandler = require("./middlewares/errorHandler");
 
 app.use(express.json());
 app.use("/api/blogs", blogsRouter);
 app.use("/api/users", usersRouter);
+app.use("/api/login", loginRouter);
 app.use(errorHandler);
 
 module.exports = app;
---
+++ blogilista/controllers/login.js
@@ -0,0 +1,39 @@
+require("express-async-errors");
+const jwt = require("jsonwebtoken");
+const router = require("express").Router();
+
+const { ValueError } = require("../utils/errors");
+const { SECRET } = require("../utils/config");
+const User = require("../models/user");
+
+router.post("/", async (request, response) => {
+  const username = request.body.username;
+  const password = request.body.password;
+
+  if (!username) throw ValueError("username");
+  if (!password) throw ValueError("password");
+
+  const user = await User.findOne({
+    where: {
+      username: username,
+    },
+  });
+  if (!user) throw ValueError("username");
+
+  const passwordCorrect = password === "salainen";
+
+  if (!passwordCorrect) throw ValueError("password");
+
+  const userForToken = {
+    username: user.username,
+    id: user.id,
+  };
+
+  const token = jwt.sign(userForToken, SECRET);
+
+  response
+    .status(200)
+    .send({ token, username: user.username, name: user.name });
+});
+
+module.exports = router;
