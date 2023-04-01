e9051e9dd6f3fca49f933775bad5c7a0aef49d4b
Oskari Juvakka <oskari.juvakka@gmail.com>
2023-04-01 15:11:44 +0300

    13.19: fix default value of read status, rename readinglist table

--- blogilista/migrations/20230401_00_add_reading_lists.js
+++ blogilista/migrations/20230401_00_add_reading_lists.js
@@ -2,30 +2,30 @@ const { DataTypes } = require("sequelize");
 
 module.exports = {
   up: async ({ context: queryInterface }) => {
-    await queryInterface.createTable("readinglist", {
+    await queryInterface.createTable("readings", {
       id: {
         type: DataTypes.INTEGER,
         primaryKey: true,
         autoIncrement: true,
       },
-      userId: {
+      user_id: {
         type: DataTypes.INTEGER,
         allowNull: false,
         references: { model: "users", key: "id" },
       },
-      blogId: {
+      blog_id: {
         type: DataTypes.INTEGER,
         allowNull: false,
         references: { model: "blogs", key: "id" },
       },
       read: {
         type: DataTypes.BOOLEAN,
+        defaultValue: false,
         allowNull: false,
-        default: false,
       },
     });
   },
   down: async ({ context: queryInterface }) => {
-    await queryInterface.dropTable("readinglist");
+    await queryInterface.dropTable("readings");
   },
 };
